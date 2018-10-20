87d7edb5505e6dd87fa2cf038138cf2581f3683d
justinrule99 <justinrule99@gmail.com>
2018-10-20 15:54:02 -0500

    some changes

--- hackisu/src/components/Container.vue
+++ hackisu/src/components/Container.vue
@@ -3,9 +3,6 @@
 	Game Logic and Content
 	Import correct images
 	Button Styling/Images
-
-
-
  -->
 
 <template>
@@ -20,7 +17,62 @@
 
 		<div class="textarea">
 			<div class="gametextcontainer">
-				<vue-typer text='playername' class="gametext" :type-delay='40'></vue-typer>
+<!-- :text= '[playername + ", .. you have awoken in the octocat universe.. a real cat-astrophe"]' -->
+
+				<vue-typer 
+					
+					v-bind:text="playername"
+					@completed='showNextLine'
+					v-if="playername"
+					class="gametext" 
+					:repeat='0'
+					:type-delay='60'
+					:erase-on-complete='true'
+					:pre-erase-delay='500'
+					erase-style='clear'>
+				</vue-typer>
+				<vue-typer text= "This is your adventure, continue forward how you wish"
+					v-if="lineComplete"
+					@completed='showNextLine'
+					:repeat='0'
+					class="gametext" 
+					:type-delay='60'
+					:erase-on-complete='false'
+					:pre-erase-delay='500'
+					erase-style='clear'>
+				</vue-typer>
+				<vue-typer text= "Choose Wisely.."
+					v-if="lineComplete && lineNumber > 1"
+					@completed='showNextLine'
+					:repeat='0'
+					class="gametext" 
+					:type-delay='60'
+					:erase-on-complete='false'
+					:pre-erase-delay='500'
+					erase-style='clear'>
+				</vue-typer>
+
+				<!-- modify to only include 1 vue-typer -->
+				<!-- <vue-typer :text= '[story + ", .. you have awoken in the octocat universe.. a real cat-astrophe"]'
+					@completed='showNextLine'
+					v-if="playername"
+					v-bind:story="['first line', 'second line', 'third line']"
+					class="gametext" 
+					:repeat='0'
+					:type-delay='60'
+					:erase-on-complete='true'
+					:pre-erase-delay='500'
+					erase-style='clear'>
+				</vue-typer>
+				<vue-typer text= "This is your adventure, continue forward how you wish"
+					v-if="lineComplete"
+					:repeat='0'
+					class="gametext" 
+					:type-delay='60'
+					:erase-on-complete='false'
+					:pre-erase-delay='500'
+					erase-style='clear'>
+				</vue-typer>  -->
 			</div>
 			
 
@@ -55,24 +107,33 @@ import { VueTyper } from 'vue-typer'
 export default {
   name: 'Container',
   components: {
-  	VueTyper
+	VueTyper
   },
   props: {
     gameRunning: false,
     nameInputVisible: false,
+    lineComplete: false,
+    lineNumber: 1,
     //menuVisible: true,
-    playername: String
+    playername: String,
+    story: String
   },
   methods: {
     startGame: function(){
       this.gameRunning = !this.gameRunning;
       this.nameInputVisible = true;
+      //this.story[0] = "welcome to the game ya idiot"
       //this.menuVisible = false
       //console.log("started forreal")
     },
     disappear: function(){
       this.nameInputVisible = false
       //this.menuVisible = true
+      //start rolling text
+    },
+    showNextLine: function(){
+		this.lineComplete = true;
+		this.lineNumber++;
     }
   }
 
