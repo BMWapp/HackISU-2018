27343ea47b470e454532fb43e03c357cbd3d2ed1
justinrule99 <justinrule99@gmail.com>
2018-10-19 20:39:20 -0500

    Migrating from Node to Vue

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 README 
 
 Project Ideas:
+Text and Image based RPG/Choose your own adventure in JS
+	Using Vue.JS and/or p5.js 
+	NOT node
---
+++ hackisu/src/components/MainComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Welcome to The App
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./MainComp.css" scoped>
+
+</style>
